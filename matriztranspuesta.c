f165fc279261d199637c0fd27f6ba4d470dd2c14
Laboratorio <laboratorio@udla-ec.int>
2023-05-22 16:33:06 -0500

    Segunda versiòn con numeros aleatorios y la transpuesta , pero tiene un error ya que no genera numeros aleatorios para todas las posiciones

--- matriztranspuesta.c
+++ matriztranspuesta.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-
+#include <time.h>
+#include <stdlib.h>
 
 int main() {
     int m, n ;
     int i , j ;
     int matriz[i][j];
+
+     srand(time(NULL));
 
     printf("MATRIZ M X N\n");
     printf("Ingrese el numero de filas de la matriz: ");
@@ -12,13 +15,12 @@ int main() {
     printf("Ingrese el numero de columnas de la matriz: ");
     scanf("%d", &n);
     
-
+     
+       srand(time(NULL));
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-             matriz[i][j] = 0;
-         
-        }  
-
+         matriz[i][j] = rand()%10; 
+    }
     }
 
      for (int i = 0; i < m; i++) {
@@ -29,8 +31,20 @@ int main() {
 
     }
 
+    printf("Transpuesta de la matriz:\n");
+     for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matriz[j][i]);
+        }
+        printf("\n");
+
+    }
+
+  
+
 
   
     
     return 0;
 }
+
